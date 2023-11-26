4c61c1c17a5c17d1ab4edd1e3cd12763a814be31
BackenderDev <bakdauletabdumuminov@gmail.com>
2023-11-26 20:45:42 +0600

    i change connectionpool class

--- src/main/java/com/bakdaulet/spring/database/pool/ConnectionPool.java
+++ src/main/java/com/bakdaulet/spring/database/pool/ConnectionPool.java
@@ -19,6 +19,7 @@ public class ConnectionPool{
 
     @PostConstruct
     private void init() {
+        System.out.println("i am groot");
         System.out.println("VRNEJKFEW");
         System.out.println("Init connection pool");
     }
