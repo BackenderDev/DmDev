adb329f7c2a25a02b0cb7047fa75866169a92694
BackenderDev <bakdauletabdumuminov@gmail.com>
2023-11-27 18:54:21 +0600

    i add class NamePool

---
+++ src/main/java/com/bakdaulet/spring/database/pool/NamePool.java
@@ -0,0 +1,7 @@
+package com.bakdaulet.spring.database.pool;
+
+public class NamePool {
+    public static void main(String[] args) {
+        System.out.println("Hello from add-feature1 branch");
+    }
+}
