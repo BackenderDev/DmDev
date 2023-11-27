a978240c4a0ec500b6d99213ca7a5c7271ad3f88
BackenderDev <bakdauletabdumuminov@gmail.com>
2023-11-27 23:31:48 +0600

    bag fixed

--- src/main/java/com/bakdaulet/spring/database/pool/NamePool.java
+++ src/main/java/com/bakdaulet/spring/database/pool/NamePool.java
@@ -2,6 +2,5 @@ package com.bakdaulet.spring.database.pool;
 
 public class NamePool {
     public static void main(String[] args) {
-        System.out.println("Hello from add-feature1 branch");
     }
 }
