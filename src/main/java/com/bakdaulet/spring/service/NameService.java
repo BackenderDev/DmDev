cd990c853704014dd7b492e7425300e80335c3ba
BackenderDev <bakdauletabdumuminov@gmail.com>
2023-11-28 17:44:27 +0600

    i solve merge

--- src/main/java/com/bakdaulet/spring/service/NameService.java
+++ src/main/java/com/bakdaulet/spring/service/NameService.java
@@ -2,10 +2,7 @@ package com.bakdaulet.spring.service;
 
 public class NameService {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("fnerjkwv");
-=======
         System.out.println("efj rjs");
->>>>>>> a7f824ab67a2f32a66b0e26a2393e4c1f4692bf7
     }
 }
