ae082ad44298366ed475816db2d050f79541fb8b
BackenderDev <bakdauletabdumuminov@gmail.com>
2023-11-28 17:55:00 +0600

    i write in class NamePool

--- src/main/java/com/bakdaulet/spring/database/pool/NamePool.java
+++ src/main/java/com/bakdaulet/spring/database/pool/NamePool.java
@@ -3,5 +3,6 @@ package com.bakdaulet.spring.database.pool;
 public class NamePool {
     public static void main(String[] args) {
         System.out.println("My name is baxa");
+        System.out.println("I am groot!");
     }
 }
