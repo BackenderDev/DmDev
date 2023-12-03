2730562bdc870ebf3e2b975cd9d07964ae352e2b
BackenderDev <bakdauletabdumuminov@gmail.com>
2023-12-03 15:04:52 +0600

    lesson 38

--- src/main/java/com/bakdaulet/spring/database/pool/NamePool.java
+++
@@ -1,7 +0,0 @@
-package com.bakdaulet.spring.database.pool;
-
-public class NamePool {
-    public static void main(String[] args) {
-        System.out.println("from main");
-    }
-}
--- src/main/java/com/bakdaulet/spring/service/NameService.java
+++
@@ -1,7 +0,0 @@
-package com.bakdaulet.spring.service;
-
-public class NameService {
-    public static void main(String[] args) {
-
-    }
-}
